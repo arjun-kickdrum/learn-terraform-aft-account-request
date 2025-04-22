2040623f2d7224bc6fa5cd5e3c507398765f98a8
arjun <arjunb.r@kickdrumtech.com>
2025-04-22 17:15:09 +0530

    add another test account

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ module "sandbox" {
   control_tower_parameters = {
     AccountEmail              = "jating32+sandbox@gmail.com"
     AccountName               = "sandbox-aft-new"
-    ManagedOrganizationalUnit = "Tenants"
+    ManagedOrganizationalUnit = "Learn AFT"
     SSOUserEmail              = "arjunb.r@kickdrumtech.com"
     SSOUserFirstName          = "arjun"
     SSOUserLastName           = "br"
@@ -33,6 +33,35 @@ module "tenantacc" {
   control_tower_parameters = {
     AccountEmail              = "jating32+tenantacc@gmail.com"
     AccountName               = "tenant-acc-new"
+    ManagedOrganizationalUnit = "Tenants"
+    SSOUserEmail              = "arjunb.r@kickdrumtech.com"
+    SSOUserFirstName          = "arjun"
+    SSOUserLastName           = "br"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
+
+module "tenantacc2" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "jating32+tenantacc2@gmail.com"
+    AccountName               = "tenant-acc-new2"
     ManagedOrganizationalUnit = "Tenants"
     SSOUserEmail              = "arjunb.r@kickdrumtech.com"
     SSOUserFirstName          = "arjun"
