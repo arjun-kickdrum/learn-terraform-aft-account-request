a2999fa6e67c1f71107b3ab9af6050303c71893e
arjun <arjunb.r@kickdrumtech.com>
2025-04-22 17:55:58 +0530

    remove failing account request

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,3 @@
-module "sandbox" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "jating32+sandbox@gmail.com"
-    AccountName               = "sandbox-aft-new"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "arjunb.r@kickdrumtech.com"
-    SSOUserFirstName          = "arjun"
-    SSOUserLastName           = "br"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
-}
-
-
 module "tenantacc" {
   source = "./modules/aft-account-request"
 
