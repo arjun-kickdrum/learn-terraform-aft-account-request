a83d1fecfcefadd145622808aa2674c7e1941bb7
arjun <arjunb.r@kickdrumtech.com>
2025-04-22 18:09:34 +0530

    new acc

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,3 +54,32 @@ module "tenantacc2" {
 
   account_customizations_name = "sandbox"
 }
+
+
+module "tenantacc3" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "jating32+tenantacc3@gmail.com"
+    AccountName               = "tenant-acc-new3"
+    ManagedOrganizationalUnit = "Tenants"
+    SSOUserEmail              = "arjunb.r@kickdrumtech.com"
+    SSOUserFirstName          = "arjun"
+    SSOUserLastName           = "br"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
