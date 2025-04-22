c1c31bd8dbbbd93ac20dd8f4dfae230fb52517f7
arjun <arjunb.r@kickdrumtech.com>
2025-04-22 19:51:07 +0530

    delete accs

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,64 +27,6 @@ module "tenantacc" {
 }
 
 
-module "tenantacc2" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "jating32+tenantacc2@gmail.com"
-    AccountName               = "tenant-acc-new2"
-    ManagedOrganizationalUnit = "Tenants"
-    SSOUserEmail              = "arjunb.r@kickdrumtech.com"
-    SSOUserFirstName          = "arjun"
-    SSOUserLastName           = "br"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
-}
-
-
-module "tenantacc3" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "jating32+tenantacc3@gmail.com"
-    AccountName               = "tenant-acc-new3"
-    ManagedOrganizationalUnit = "Tenants"
-    SSOUserEmail              = "arjunb.r@kickdrumtech.com"
-    SSOUserFirstName          = "arjun"
-    SSOUserLastName           = "br"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
-}
-
-
 module "tenantacc4" {
   source = "./modules/aft-account-request"
 
